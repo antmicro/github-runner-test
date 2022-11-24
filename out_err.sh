5554318322c5561f509146bf7e9743ce8143ecd1
Adam Olech <aolech@antmicro.com>
2022-11-24 13:46:16 +0100

    Remove out_err.sh

--- out_err.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echoerr() { echo "$@" 1>&2; }
-
-for i in {1..10}; do
-    echo "[$i] Hello from stdout!"
-    echoerr "[$i] Hello from stderr..."
-    echoerr "[$i] second hello from stderr"
-done
